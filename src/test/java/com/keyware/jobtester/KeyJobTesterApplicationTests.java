74953f0dc495c376cf32d918218e5aadf862213c
GuoXin <371864209@qq.com>
2022-06-13 09:53:46 +0800

    Initial commit

---
+++ src/test/java/com/keyware/jobtester/KeyJobTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyware.jobtester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyJobTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
